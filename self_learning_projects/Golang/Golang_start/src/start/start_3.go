320daa4334b5ee3622c56964d5d794f518fdaddc
yd_zhang <petenlf1025@gmail.com>
2018-01-31 13:35:24 +0800

    keep learning Golang

--- self_learning_projects/Golang/Golang_start/src/start/start_3.go
+++ self_learning_projects/Golang/Golang_start/src/start/start_3.go
@@ -199,4 +199,102 @@ c <-chan string  // 只能从channel读取数据
 c chan string  // 默认的定义方式，则既可以想channel写入数据，也可以从channel读取数据
 */
 
+// package main
 
+// import (
+//     "fmt"
+//     "time"
+// )
+
+// func fixed_shooting(msg_chan chan string) {
+//     for {
+//         msg_chan <- "fixed shooting"
+//         time.Sleep(time.Second * 1)
+//     }
+// }
+
+// func three_point_shooting(msg_chan chan string) {
+//     for {
+//         msg_chan <- "three point shooting"
+//         time.Sleep(time.Second * 1)
+//     }
+// }
+
+// func main() {
+//     c_fixed := make(chan string)
+//     c_3_point := make(chan string)
+
+//     go fixed_shooting(c_fixed)
+//     go three_point_shooting(c_3_point)
+
+//     go func() {
+//         for {
+//             select {
+//             case msg1 := <-c_fixed:
+//                 fmt.Println(msg1)
+//             case msg2 := <-c_3_point:
+//                 fmt.Println(msg2)
+//             }
+//         }
+//     }()
+
+//     var input string
+//     fmt.Scanln(&input)
+//     fmt.Println("----over----")
+// }
+
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func fixed_shooting(msg_chan chan string) {
+    var times = 3
+    var t = 1
+    for {
+        if t <= times {
+            msg_chan <- "fixed shooting"
+        }
+        t++
+        time.Sleep(time.Second * 1)
+    }
+}
+
+func three_point_shooting(msg_chan chan string) {
+    var times = 5
+    var t = 1
+    for {
+        if t <= times {
+            msg_chan <- "three point shooting"
+        }
+        t++
+        time.Sleep(time.Second * 1)
+    }
+}
+
+func main() {
+    c_fixed := make(chan string)
+    c_3_point := make(chan string)
+
+    go fixed_shooting(c_fixed)
+    go three_point_shooting(c_3_point)
+
+    go func() {
+        for {
+            select {
+            case msg1 := <-c_fixed:
+                fmt.Println(msg1)
+            case msg2 := <-c_3_point:
+                fmt.Println(msg2)
+            case <- time.After(time.Second * 5):
+                fmt.Println("timeout, check again...")
+            }
+        }
+    }()
+
+    var input string
+    fmt.Scanln(&input)
+    fmt.Println("----over----")
+}
