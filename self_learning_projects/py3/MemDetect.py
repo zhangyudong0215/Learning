11ee6ebb08286be8febaa45f7257a20d19eda8e7
yd_zhang <petenlf1025@gmail.com>
2018-02-01 15:47:55 +0800

    规范代码格式

--- self_learning_projects/py3/MemDetect.py
+++ self_learning_projects/py3/MemDetect.py
@@ -89,10 +89,13 @@ class MemDetect():
         mem = psutil.virtual_memory()
         localtime = time.asctime(time.localtime(time.time()))
         print(localtime)
-        print('Total: %.2fGB\tFree: %.2fGB %.2f%%\tAvailable: %.2fGB %.2f%%\tMEM_USE: %.2fGB %.2f%%' % 
-               (mem.total/1024**3, mem.free/1024**3, mem.free/mem.total*100, mem.available/1024**3, 
-                mem.available/mem.total*100, mem.used/1024**3, mem.used/mem.total*100))
-        print("进程名称: %s\t内存占用百分比: %.2f%%" %(p.name(), p.memory_percent()))
+        print('Total: %.2fGB\tFree: %.2fGB %.2f%%\t \
+            Available: %.2fGB %.2f%%\tMEM_USE: %.2fGB %.2f%%' % 
+               (mem.total/1024**3, mem.free/1024**3, mem.free/mem.total*100, 
+                mem.available/1024**3, mem.available/mem.total*100, 
+                mem.used/1024**3, mem.used/mem.total*100))
+        print("进程名称: %s\t内存占用百分比: %.2f%%" 
+            %(p.name(), p.memory_percent()))
 
     def detect_main(self):
         while os.path.isfile('/proc/%s/stat' %str(self.pid)):
@@ -105,10 +108,13 @@ def mem_detect(pid):
     mem = psutil.virtual_memory()
     localtime = time.asctime(time.localtime(time.time()))
     print(localtime)
-    print('Total: %.2fGB\tFree: %.2fGB %.2f%%\tAvailable: %.2fGB %.2f%%\tMEM_USE: %.2fGB %.2f%%' % 
-           (mem.total/1024**3, mem.free/1024**3, mem.free/mem.total*100, mem.available/1024**3, 
-            mem.available/mem.total*100, mem.used/1024**3, mem.used/mem.total*100))
-    print("进程名称: %s\t内存占用百分比: %.2f%%" %(p.name(), p.memory_percent()))
+    print('Total: %.2fGB\tFree: %.2fGB %.2f%%\t \
+        Available: %.2fGB %.2f%%\tMEM_USE: %.2fGB %.2f%%' % 
+           (mem.total/1024**3, mem.free/1024**3, mem.free/mem.total*100, 
+            mem.available/1024**3, mem.available/mem.total*100, 
+            mem.used/1024**3, mem.used/mem.total*100))
+    print("进程名称: %s\t内存占用百分比: %.2f%%" 
+        %(p.name(), p.memory_percent()))
 
 def detect_main(**kwargs):
     while os.path.isfile('/proc/%s/stat' %str(kwargs['pid'])):
