78ddd25d57a587ffe262553f7946d02781a5ac6b
yd_zhang <petenlf1025@gmail.com>
2018-02-07 17:27:20 +0800

    complete polar2cartesian

--- self_learning_projects/Golang/Golang_start/src/start/gobook_1.go
+++ self_learning_projects/Golang/Golang_start/src/start/gobook_1.go
@@ -181,4 +181,77 @@
 //     }, nil
 // }
 
+package main
 
+import (
+    "bufio"
+    "fmt"
+    "math"
+    "os"
+    "runtime"
+)
+
+type polar struct {
+    radius float64
+    theta float64
+}
+
+type cartesian struct {
+    x float64
+    y float64
+}
+
+var prompt = "Enter a radius and an angle (in degrees), e.g., 12.5 90, " + "or %s to quit."
+
+func init() {
+    if runtime.GOOS == "windows" {
+        prompt = fmt.Sprintf(prompt, "Ctrl+Z, Enter")
+    } else { //类 Unix
+        prompt = fmt.Sprintf(prompt, "Ctrl+D")
+    }
+}
+
+func main() {
+    questions := make(chan polar)
+    defer close(questions)
+    answers := createSolver(questions)
+    defer close(answers)
+    interact(questions, answers)
+}
+
+func createSolver(questions chan polar) chan cartesian {
+    answers := make(chan cartesian)
+    go func() {
+        for {
+            polarCoord := <-questions
+            theta := polarCoord.theta * math.Pi / 180.0
+            x := polarCoord.radius * math.Cos(theta)
+            y := polarCoord.radius * math.Sin(theta)
+            answers <- cartesian{x, y}
+        }
+    }()
+    return answers
+}
+
+const result = "Polar radius=%.02f theta=%.02f° -> Cartesian x=%.02f y=%.02f\n"
+
+func interact(questions chan polar, answers chan cartesian) {
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Println(prompt)
+    for {
+        fmt.Println("Radius and angle: ")
+        line, err := reader.ReadString('\n')
+        if err != nil {
+            break
+        }
+        var radius, theta float64
+        if _, err := fmt.Sscanf(line, "%f %f", &radius, &theta); err != nil {  // 书上的代码是fmt.Sscan, 无法正常读取
+            fmt.Println(os.Stderr, "invalid input")
+            continue
+        }
+        questions <- polar{radius, theta}
+        corrd := <-answers
+        fmt.Printf(result, radius, theta, corrd.x, corrd.y)
+    }
+    fmt.Println()
+}
