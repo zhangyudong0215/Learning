e368a089429abef31798f32e873f8257fe0d0a1e
yd_zhang <petenlf1025@gmail.com>
2018-02-12 21:07:17 +0800

    add a practice

--- self_learning_projects/Golang/Golang_start/src/start/gobook_1.go
+++ self_learning_projects/Golang/Golang_start/src/start/gobook_1.go
@@ -398,12 +398,160 @@
 // 	return result
 // }
 
-package main
+// // 在上述代码基础上实现估算众数和标准差，众数自己写的，标准差参考答案
 
-import (
-	"fmt"
-)
+// package main
 
-func main() {
-	fmt.Println("hello world")
-}
+// import (
+// 	"fmt"
+// 	"log"
+// 	"math"
+// 	"net/http"
+// 	"sort"
+// 	"strconv"
+// 	"strings"
+// )
+
+// const (
+// 	pageTop = `<!DOCTYPE HTML><html><head>
+// <style>.error{color:#FF0000;}</style></head><title>Statistics</title>
+// <body><h3>Statistics</h3>
+// <p>Computes basic statistics for a given list of numbers</p>`
+// 	form = `<form action="/" method="POST">
+// <label for="numbers">Numbers (comma or space-separated):</label><br />
+// <input type="text" name="numbers" size="30"><br />
+// <input type="submit" value="Calculate">
+// </form>`
+// 	pageBottom = `</body></html>`
+// 	anError    = `<p class="error">%s</p>`
+// )
+
+// type statistics struct {
+// 	numbers []float64
+// 	mean    float64
+// 	median  float64
+// 	modes   []float64 // 众数
+// 	std     float64   // 标准差
+// }
+
+// func main() {
+// 	http.HandleFunc("/", homePage)
+// 	if err := http.ListenAndServe(":9001", nil); err != nil {
+// 		log.Fatal("failed to start server", err)
+// 	}
+// }
+
+// func homePage(writer http.ResponseWriter, request *http.Request) {
+// 	err := request.ParseForm() // Must be called before writing response
+// 	fmt.Fprint(writer, pageTop, form)
+// 	if err != nil {
+// 		fmt.Fprintf(writer, anError, err)
+// 	} else {
+// 		if numbers, message, ok := processRequest(request); ok {
+// 			stats := getStats(numbers)
+// 			fmt.Fprint(writer, formatStats(stats))
+// 		} else if message != "" {
+// 			fmt.Fprintf(writer, anError, message)
+// 		}
+// 	}
+// 	fmt.Fprint(writer, pageBottom)
+// }
+
+// func processRequest(request *http.Request) ([]float64, string, bool) {
+// 	var numbers []float64
+// 	if slice, found := request.Form["numbers"]; found && len(slice) > 0 {
+// 		text := strings.Replace(slice[0], ",", " ", -1)
+// 		for _, field := range strings.Fields(text) {
+// 			if x, err := strconv.ParseFloat(field, 64); err != nil {
+// 				return numbers, "'" + field + "' is invalid", false
+// 			} else {
+// 				numbers = append(numbers, x)
+// 			}
+// 		}
+// 	}
+// 	if len(numbers) == 0 {
+// 		return numbers, "", false // no data first time form is shown
+// 	}
+// 	return numbers, "", true
+// }
+
+// func formatStats(stats statistics) string {
+// 	return fmt.Sprintf(`<table border="1">
+// <tr><th colspan="2">Results</th></tr>
+// <tr><td>Numbers</td><td>%v</td></tr>
+// <tr><td>Count</td><td>%d</td></tr>
+// <tr><td>Mean</td><td>%f</td></tr>
+// <tr><td>Median</td><td>%f</td></tr>
+// <tr><td>Modes</td><td>%v</td></tr>
+// <tr><td>Std</td><td>%f</td></tr>
+// </table>`, stats.numbers, len(stats.numbers), stats.mean, stats.median,
+// 		stats.modes, stats.std)
+// }
+
+// func getStats(numbers []float64) (stats statistics) {
+// 	stats.numbers = numbers
+// 	sort.Float64s(stats.numbers)
+// 	stats.mean = sum(numbers) / float64(len(numbers))
+// 	stats.median = median(numbers)
+// 	stats.modes = modes(numbers)
+// 	stats.std = std(numbers)
+// 	return stats
+// }
+
+// func sum(numbers []float64) (total float64) {
+// 	for _, x := range numbers {
+// 		total += x
+// 	}
+// 	return total
+// }
+
+// func median(numbers []float64) float64 {
+// 	middle := len(numbers) / 2
+// 	result := numbers[middle]
+// 	if len(numbers)%2 == 0 {
+// 		result = (result + numbers[middle-1]) / 2
+// 	}
+// 	return result
+// }
+
+// func std(numbers []float64) float64 {
+// 	var mean float64 = sum(numbers) / float64(len(numbers))
+// 	var len_ float64 = float64(len(numbers) - 1)
+// 	var sum_ float64
+// 	for _, num := range numbers {
+// 		sum_ += math.Pow((num - mean), 2)
+// 	}
+// 	return math.Sqrt(sum_ / len_)
+// }
+
+// func modes(numbers []float64) (modes []float64) {
+// 	frequencies := make(map[float64]int, len(numbers))
+// 	highestFrequency := 0
+// 	for _, x := range numbers {
+// 		frequencies[x]++
+// 		if frequencies[x] > highestFrequency {
+// 			highestFrequency = frequencies[x]
+// 		}
+// 	}
+// 	for x, frequency := range frequencies {
+// 		if frequency == highestFrequency {
+// 			modes = append(modes, x)
+// 		}
+// 	}
+// 	if highestFrequency == 1 || len(modes) == len(frequencies) {
+// 		modes = modes[:0] // Or: modes = []float64{}
+// 	}
+// 	sort.Float64s(modes)
+// 	return modes
+// }
+
+// /*  mode:
+//     numbers                               expected
+//     [1, 2, 3, 4, 5, 6]                    []
+//     [1, 2, 2, 3, 3, 3, 4, 4, 5]           [3]
+//     [9, 9, 9]                             []
+//     [1, 1, 2, 2]                          []
+//     [2, 2, 2, 4, 4, 4, 8, 8]              [2, 4]
+//     [1, 3, 6, 6, 6, 6, 7, 7, 12, 12, 17]  [6]
+//     [1, 1, 2, 4, 4]                       [1, 4]
+// */
