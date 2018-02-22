3aa208c0f6c0544448bac4ff4cb22d1b4833c7a1
yd_zhang <petenlf1025@gmail.com>
2018-02-22 22:20:20 +0800

    finish a subset of the exercises of chapter4

--- self_learning_projects/Golang/Golang_start/src/start/gobook_2.go
+++ self_learning_projects/Golang/Golang_start/src/start/gobook_2.go
@@ -233,127 +233,195 @@
 // 	}
 // }
 
+// package main
+
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"io"
+// 	"log"
+// 	"os"
+// 	"path/filepath"
+// 	"runtime"
+// 	"sort"
+// 	"strings"
+// 	"unicode"
+// 	"unicode/utf8"
+// )
+
+// func main() {
+// 	if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+// 		fmt.Printf("usage: %s <file1> [<file2> [... <fileN>]]\n",
+// 			filepath.Base(os.Args[0]))
+// 		os.Exit(1)
+// 	}
+// 	frequencyForWord := map[string]int{}
+// 	for _, filename := range commandLineFiles(os.Args[1:]) {
+// 		updateFrequencies(filename, frequencyForWord)
+// 	}
+// 	reportByWords(frequencyForWord)
+// 	wordsForFrequency := invertStringIntMap(frequencyForWord)
+// 	reportByFrequency(wordsForFrequency)
+// }
+
+// func commandLineFiles(files []string) []string {
+// 	if runtime.GOOS == "windows" {
+// 		args := make([]string, 0, len(files))
+// 		for _, name := range files {
+// 			if matches, err := filepath.Glob(name); err != nil {
+// 				args = append(args, name)
+// 			} else if matches != nil {
+// 				args = append(args, matches...)
+// 			}
+// 		}
+// 		return args
+// 	}
+// 	return files
+// }
+
+// func updateFrequencies(filename string, frequencyForWord map[string]int) {
+// 	var file *os.File
+// 	var err error
+// 	if file, err = os.Open(filename); err != nil {
+// 		log.Println("failed to open the file: ", err)
+// 		return
+// 	}
+// 	defer file.Close()
+// 	readAndUpdateFrequencies(bufio.NewReader(file), frequencyForWord)
+// }
+
+// func readAndUpdateFrequencies(reader *bufio.Reader, frequencyForWord map[string]int) {
+// 	for {
+// 		line, err := reader.ReadString('\n')
+// 		for _, word := range SplitOnNonLetters(strings.TrimSpace(line)) {
+// 			if len(word) > utf8.UTFMax || utf8.RuneCountInString(word) > 1 { // 至少两个字母的单词
+// 				frequencyForWord[strings.ToLower(word)] += 1
+// 			}
+// 		}
+// 		if err != nil {
+// 			if err != io.EOF {
+// 				log.Println("failed to finish reading the file: ", err)
+// 			}
+// 			break
+// 		}
+// 	}
+// }
+
+// func SplitOnNonLetters(s string) []string {
+// 	notALetter := func(char rune) bool {
+// 		return !unicode.IsLetter(char)
+// 	}
+// 	return strings.FieldsFunc(s, notALetter)
+// }
+
+// func reportByWords(frequencyForWord map[string]int) {
+// 	words := make([]string, 0, len(frequencyForWord))
+// 	wordWidth, frequencyWidth := 0, 0
+// 	for word, frequency := range frequencyForWord {
+// 		words = append(words, word)
+// 		if width := utf8.RuneCountInString(word); width > wordWidth {
+// 			wordWidth = width
+// 		}
+// 		if width := len(fmt.Sprint(frequency)); width > frequencyWidth {
+// 			frequencyWidth = width
+// 		}
+// 	}
+// 	sort.Strings(words)
+// 	gap := wordWidth + frequencyWidth - len("Word") - len("Frequency")
+// 	fmt.Printf("word %*s%s\n", gap, " ", "Frequency")
+// 	for _, word := range words {
+// 		fmt.Printf("%-*s %*d\n", wordWidth, word, frequencyWidth,
+// 			frequencyForWord[word])
+// 	}
+// }
+
+// func invertStringIntMap(intForString map[string]int) map[int][]string {
+// 	stringsForInt := make(map[int][]string, len(intForString))
+// 	for key, value := range intForString {
+// 		stringsForInt[value] = append(stringsForInt[value], key)
+// 	}
+// 	return stringsForInt
+// }
+
+// func reportByFrequency(wordsForFrequency map[int][]string) {
+// 	frequencies := make([]int, 0, len(wordsForFrequency))
+// 	for frequency := range wordsForFrequency {
+// 		frequencies = append(frequencies, frequency)
+// 	}
+// 	sort.Ints(frequencies)
+// 	width := len(fmt.Sprint(frequencies[len(frequencies)-1]))
+// 	fmt.Println("Frequency -> Words")
+// 	for _, frequency := range frequencies {
+// 		words := wordsForFrequency[frequency]
+// 		sort.Strings(words)
+// 		fmt.Printf("%*d %s\n", width, frequency, strings.Join(words, ", "))
+// 	}
+// }
+
+// chapter 4 exercises
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"log"
-	"os"
-	"path/filepath"
-	"runtime"
-	"sort"
-	"strings"
-	"unicode"
-	"unicode/utf8"
+    "fmt"
 )
 
 func main() {
-	if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
-		fmt.Printf("usage: %s <file1> [<file2> [... <fileN>]]\n",
-			filepath.Base(os.Args[0]))
-		os.Exit(1)
-	}
-	frequencyForWord := map[string]int{}
-	for _, filename := range commandLineFiles(os.Args[1:]) {
-		updateFrequencies(filename, frequencyForWord)
-	}
-	reportByWords(frequencyForWord)
-	wordsForFrequency := invertStringIntMap(frequencyForWord)
-	reportByFrequency(wordsForFrequency)
+    irregularMatrix := [][]int{{1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11},
+        {12, 13, 14, 15},
+        {16, 17, 18, 19, 20}}
+    fmt.Println("irregular:", irregularMatrix)
+    slice := Flatten(irregularMatrix)
+    fmt.Printf("1x%d: %v\n", len(slice), slice)
+    fmt.Printf(" 3x%v\n", Make2D(slice, 3))
+    fmt.Printf(" 4x%v\n", Make2D(slice, 4))
+    fmt.Printf(" 5x%v\n", Make2D(slice, 5))
+    fmt.Printf(" 6x%v\n", Make2D(slice, 6))
+    slice = []int{9, 1, 9, 5, 4, 4, 2, 1, 5, 4, 8, 8, 4, 3, 6, 9, 5, 7, 5}
+    fmt.Println("Original:", slice)
+    slice = UniqueInts(slice)
+    fmt.Println("Unique:  ", slice)
 }
 
-func commandLineFiles(files []string) []string {
-	if runtime.GOOS == "windows" {
-		args := make([]string, 0, len(files))
-		for _, name := range files {
-			if matches, err := filepath.Glob(name); err != nil {
-				args = append(args, name)
-			} else if matches != nil {
-				args = append(args, matches...)
-			}
-		}
-		return args
-	}
-	return files
+//----1----
+func UniqueInts(slice []int) []int {
+    ans := []int{}
+    seen := map[int]bool{}
+    for _, num := range slice {
+        if _, found := seen[num]; !found {
+            seen[num] = true
+            ans = append(ans, num)
+        }
+    }
+    return ans
 }
 
-func updateFrequencies(filename string, frequencyForWord map[string]int) {
-	var file *os.File
-	var err error
-	if file, err = os.Open(filename); err != nil {
-		log.Println("failed to open the file: ", err)
-		return
-	}
-	defer file.Close()
-	readAndUpdateFrequencies(bufio.NewReader(file), frequencyForWord)
+//----2----
+func Flatten(slice2D [][]int) []int {
+    ans := []int{}
+    for _, slice := range slice2D {
+        for _, num := range slice {
+            ans = append(ans, num)
+        }
+    }
+    return ans
 }
 
-func readAndUpdateFrequencies(reader *bufio.Reader, frequencyForWord map[string]int) {
-	for {
-		line, err := reader.ReadString('\n')
-		for _, word := range SplitOnNonLetters(strings.TrimSpace(line)) {
-			if len(word) > utf8.UTFMax || utf8.RuneCountInString(word) > 1 { // 至少两个字母的单词
-				frequencyForWord[strings.ToLower(word)] += 1
-			}
-		}
-		if err != nil {
-			if err != io.EOF {
-				log.Println("failed to finish reading the file: ", err)
-			}
-			break
-		}
-	}
-}
-
-func SplitOnNonLetters(s string) []string {
-	notALetter := func(char rune) bool {
-		return !unicode.IsLetter(char)
-	}
-	return strings.FieldsFunc(s, notALetter)
-}
-
-func reportByWords(frequencyForWord map[string]int) {
-	words := make([]string, 0, len(frequencyForWord))
-	wordWidth, frequencyWidth := 0, 0
-	for word, frequency := range frequencyForWord {
-		words = append(words, word)
-		if width := utf8.RuneCountInString(word); width > wordWidth {
-			wordWidth = width
-		}
-		if width := len(fmt.Sprint(frequency)); width > frequencyWidth {
-			frequencyWidth = width
-		}
-	}
-	sort.Strings(words)
-	gap := wordWidth + frequencyWidth - len("Word") - len("Frequency")
-	fmt.Printf("word %*s%s\n", gap, " ", "Frequency")
-	for _, word := range words {
-		fmt.Printf("%-*s %*d\n", wordWidth, word, frequencyWidth,
-			frequencyForWord[word])
-	}
-}
-
-func invertStringIntMap(intForString map[string]int) map[int][]string {
-	stringsForInt := make(map[int][]string, len(intForString))
-	for key, value := range intForString {
-		stringsForInt[value] = append(stringsForInt[value], key)
-	}
-	return stringsForInt
-}
-
-func reportByFrequency(wordsForFrequency map[int][]string) {
-	frequencies := make([]int, 0, len(wordsForFrequency))
-	for frequency := range wordsForFrequency {
-		frequencies = append(frequencies, frequency)
-	}
-	sort.Ints(frequencies)
-	width := len(fmt.Sprint(frequencies[len(frequencies)-1]))
-	fmt.Println("Frequency -> Words")
-	for _, frequency := range frequencies {
-		words := wordsForFrequency[frequency]
-		sort.Strings(words)
-		fmt.Printf("%*d %s\n", width, frequency, strings.Join(words, ", "))
-	}
+//----3----
+func Make2D(slice []int, col int) [][]int {
+    ans := [][]int{}
+    row := len(slice) / col
+    if len(slice)%col != 0 {
+        row++
+    }
+    for i := 0; i < row; i++ {
+        ans = append(ans, make([]int, col))
+    }
+    for index, num := range slice {
+        r := index / col
+        c := index % col
+        ans[r][c] = num
+    }
+    return ans
 }
