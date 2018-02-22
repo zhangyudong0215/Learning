6d79e2cc7db4a6ed24189d57c9c6398453f8d0f5
yd_zhang <petenlf1025@gmail.com>
2018-02-22 20:37:42 +0800

    添加例子：判断文件分隔符

--- self_learning_projects/Golang/Golang_start/src/start/gobook_2.go
+++ self_learning_projects/Golang/Golang_start/src/start/gobook_2.go
@@ -60,87 +60,175 @@
 // 	return number
 // }
 
+// package main
+
+// import (
+// 	"fmt"
+// 	"io/ioutil"
+// 	"log"
+// 	"os"
+// 	"path/filepath"
+// 	"strconv"
+// 	"strings"
+// )
+
+// type Song struct {
+// 	Title    string
+// 	Filename string
+// 	Seconds  int
+// }
+
+// func main() {
+// 	if len(os.Args) == 1 || !strings.HasSuffix(os.Args[1], ".m3u") {
+// 		fmt.Printf("usage: %s <file.m3u>\n", filepath.Base(os.Args[0]))
+// 		os.Exit(1)
+// 	}
+// 	if rawBytes, err := ioutil.ReadFile(os.Args[1]); err != nil {
+// 		log.Fatal(err)
+// 	} else {
+// 		songs := readM3uPlaylist(string(rawBytes))
+// 		writePlsPlaylist(songs)
+// 	}
+// }
+
+// func readM3uPlaylist(data string) (songs []Song) {
+// 	var song Song
+// 	for _, line := range strings.Split(data, "\n") {
+// 		line = strings.TrimSpace(line)
+// 		if line == "" || strings.HasPrefix(line, "#EXTM3U") {
+// 			continue
+// 		}
+// 		if strings.HasPrefix(line, "#EXTINF:") {
+// 			song.Title, song.Seconds = parseExtinfLine(line)
+// 		} else {
+// 			song.Filename = strings.Map(mapPlatformDirSeparator, line)
+// 		}
+// 		if song.Filename != "" && song.Title != "" && song.Seconds != 0 {
+// 			songs = append(songs, song)
+// 			song = Song{}
+// 		}
+// 	}
+// 	return songs
+// }
+
+// func parseExtinfLine(line string) (title string, seconds int) {
+// 	if i := strings.IndexAny(line, "-0123456789"); i > -1 {
+// 		const separator = ","
+// 		line = line[i:]
+// 		if j := strings.Index(line, separator); j > -1 {
+// 			title = line[j+len(separator):]
+// 			var err error
+// 			if seconds, err = strconv.Atoi(line[:j]); err != nil {
+// 				log.Printf("failed to read the duration for '%s': %v\n", title, err)
+// 				seconds = -1
+// 			}
+// 		}
+// 	}
+// 	return title, seconds
+// }
+
+// func mapPlatformDirSeparator(char rune) rune {
+// 	if char == '/' || char == '\\' {
+// 		return filepath.Separator
+// 	}
+// 	return char
+// }
+
+// func writePlsPlaylist(songs []Song) {
+// 	fmt.Println("[playlist]")
+// 	for i, song := range songs {
+// 		i++
+// 		fmt.Printf("File%d=%s\n", i, song.Filename)
+// 		fmt.Printf("Title%d=%s\n", i, song.Title)
+// 		fmt.Printf("Length%d=%d\n", i, song.Seconds)
+// 	}
+// 	fmt.Printf("NumberOfEntries=%d\nVersion=2\n", len(songs))
+// }
+
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
-	"strconv"
 	"strings"
 )
 
-type Song struct {
-	Title    string
-	Filename string
-	Seconds  int
-}
-
 func main() {
-	if len(os.Args) == 1 || !strings.HasSuffix(os.Args[1], ".m3u") {
-		fmt.Printf("usage: %s <file.m3u>\n", filepath.Base(os.Args[0]))
+	if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+		fmt.Printf("usage: %s file\n", filepath.Base(os.Args[0]))
 		os.Exit(1)
 	}
-	if rawBytes, err := ioutil.ReadFile(os.Args[1]); err != nil {
-		log.Fatal(err)
-	} else {
-		songs := readM3uPlaylist(string(rawBytes))
-		writePlsPlaylist(songs)
-	}
+	separators := []string{"\t", "*", "|", "•"}
+	linesRead, lines := readUpToNLines(os.Args[1], 5)
+	counts := createCounts(lines, separators, linesRead)
+	separator := guessSep(counts, separators, linesRead)
+	report(separator)
 }
 
-func readM3uPlaylist(data string) (songs []Song) {
-	var song Song
-	for _, line := range strings.Split(data, "\n") {
-		line = strings.TrimSpace(line)
-		if line == "" || strings.HasPrefix(line, "#EXTM3U") {
-			continue
+func readUpToNLines(filename string, maxLines int) (int, []string) {
+	var file *os.File
+	var err error
+	if file, err = os.Open(filename); err != nil {
+		log.Fatal("fialed to open the file:", err)
+	}
+	defer file.Close()
+	lines := make([]string, maxLines)
+	reader := bufio.NewReader(file)
+	i := 0
+	for ; i < maxLines; i++ {
+		line, err := reader.ReadString('\n')
+		if line != "" {
+			lines[i] = line
 		}
-		if strings.HasPrefix(line, "#EXTINF:") {
-			song.Title, song.Seconds = parseExtinfLine(line)
-		} else {
-			song.Filename = strings.Map(mapPlatformDirSeparator, line)
-		}
-		if song.Filename != "" && song.Title != "" && song.Seconds != 0 {
-			songs = append(songs, song)
-			song = Song{}
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatal("failed to finish reading the file: ", err)
 		}
 	}
-	return songs
+	return i, lines[:i]
 }
 
-func parseExtinfLine(line string) (title string, seconds int) {
-	if i := strings.IndexAny(line, "-0123456789"); i > -1 {
-		const separator = ","
-		line = line[i:]
-		if j := strings.Index(line, separator); j > -1 {
-			title = line[j+len(separator):]
-			var err error
-			if seconds, err = strconv.Atoi(line[:j]); err != nil {
-				log.Printf("failed to read the duration for '%s': %v\n", title, err)
-				seconds = -1
+func createCounts(lines, separators []string, linesRead int) [][]int {
+	counts := make([][]int, len(separators))
+	for sepIndex := range separators {
+		counts[sepIndex] = make([]int, linesRead)
+		for lineIndex, line := range lines {
+			counts[sepIndex][lineIndex] = strings.Count(line, separators[sepIndex])
+		}
+	}
+	return counts
+}
+
+func guessSep(counts [][]int, separators []string, linesRead int) string {
+	for sepIndex := range separators {
+		same := true
+		count := counts[sepIndex][0]
+		for lineIndex := 1; lineIndex < linesRead; lineIndex++ {
+			if counts[sepIndex][lineIndex] != count {
+				same = false
+				break
 			}
 		}
+		if count > 0 && same {
+			return separators[sepIndex]
+		}
 	}
-	return title, seconds
+	return ""
 }
 
-func mapPlatformDirSeparator(char rune) rune {
-	if char == '/' || char == '\\' {
-		return filepath.Separator
+func report(separator string) {
+	switch separator {
+	case "":
+		fmt.Println("whitespace-separated or not separated at all")
+	case "\t":
+		fmt.Println("tab-separated")
+	default:
+		fmt.Printf("%s-separated\n", separator)
 	}
-	return char
-}
-
-func writePlsPlaylist(songs []Song) {
-	fmt.Println("[playlist]")
-	for i, song := range songs {
-		i++
-		fmt.Printf("File%d=%s\n", i, song.Filename)
-		fmt.Printf("Title%d=%s\n", i, song.Title)
-		fmt.Printf("Length%d=%d\n", i, song.Seconds)
-	}
-	fmt.Printf("NumberOfEntries=%d\nVersion=2\n", len(songs))
 }
